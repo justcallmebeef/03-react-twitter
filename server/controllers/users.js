98472221bda45cc2309fa31af71d4c0523ef55da
Jennifer Winer <jeniwiner@gmail.com>
2018-09-21 22:07:23 -0600

    signup route working

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,42 +1,60 @@
 const knex = require('../knex');
 const validator = require('validator');
 const bcrypt = require('bcrypt');
+const SALT_ROUNDS = 10;
 // ERRORS
 const ERROR_500 = 'Internal Server Error';
 const INV_REQ = 'Bad Handle or Password';
 
 const loginController = async (req, res, next) => {
   try {
-    if (!req.body || !req.body.handle || !req.body.password) {
-      res.err = INV_REQ;
-      return next();
-    }
-    let user = await knex('users').where('handle', req.body.handle).then(res => res[0]);
-    if (!user) {
-      res.err = INV_REQ;
-      return next();
-    }
-    let userInfo = await bcrypt.compare(req.body.password, user.password, (err, result) => {
-      if (err) {
-        res.err = INV_REQ;
-        return next();
-      }
-      return result;
-    })
+    const { handle, password } = req.body;
+    if (!handle || !password) return handleError(res, next, INV_REQ);
 
-    let { handle, email, name, avatar } = user;
-    res.data = { handle, email, name, avatar };
+    let user = await knex('users').where('handle', handle).then(res => res[0]);
+    if (!user)  return handleError(res, next, INV_REQ);
+
+    let passwordMatch = await bcrypt.compare(password, user.password);
+    if (!passwordMatch) return handleError(res, next, INV_REQ);
+
+    res.data = {
+      user: user.handle,
+      email: user.email,
+      name: user.name,
+      avatar: user.avatar };
     return next();
   } catch (err) {
-    return res.err = ERROR_500;
-    next();
+    return handleError(res, next);
   }
 };
 
-const signupController = (req, res) => {
-  res.send({ succes: true });
+const signupController = async(req, res, next) => {
+  try {
+    const { handle, name, email, avatar, password } = req.body;
+    if (!handle || !password || !name || !email || !validator.isEmail(email)) return handleError(res, next, INV_REQ);
 
-  //res.end()
+    let hashedPassword = await bcrypt.hash(password, SALT_ROUNDS);
+    if (!hashedPassword) return handleError(res, next, INV_REQ);
+
+    let userInfo = {
+      name,
+      handle,
+      email,
+      password: hashedPassword
+    }
+    if (avatar && validator.isBase64(avatar)) userInfo.avatar = avatar;
+
+    let insertedUser = await knex('users').insert(userInfo).returning(['name', 'handle', 'email', 'avatar']);
+    res.data = { user: insertedUser };
+    return next();
+  } catch (err) {
+    return handleError(res, next);
+  }
+}
+
+const handleError = (res, next, err) => {
+  res.err = err || ERROR_500;
+  return next();
 }
 
 module.exports = {
