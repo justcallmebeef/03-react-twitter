ec4b428bed0010be1c5805e44aedb3598212ec48
evapodaca87 <evapodaca87@gmail.com>
2018-10-18 19:21:52 -0600

    generated form for password

--- src/components/Login.js
+++ src/components/Login.js
@@ -8,21 +8,35 @@ class Login extends React.Component {
             password: null,
         };
         this.onChange = this.onChange.bind(this);
-
+        this.onSubmit = this.onSubmit.bind(this);
     }
 
 
     onChange (e){
+        this.setState({
+            [e.target.name]: e.target.value
+        }) 
+        
+    }
+    onSubmit (e){
         // e.target.value
+        e.preventDefault();
+        const { handle, password } = this.state
+
+        if(!handle || !password){
+            return;
+        }
+
+        console.log(handle, password)
         
     }
     render() {
       return (
-        <form>
-            <input type="text" onChange={this.onChange} value={this.state.handle}/>
-            <input type="password" onChange={this.onChange} value={this.state.password}/>
+        <form onSubmit={this.onSubmit} method="post">
+            <input name="handle" type="text" onChange={this.onChange} value={this.state.handle}/>
+            <input name="password" type="password" onChange={this.onChange} value={this.state.password}/>
             <button type="submit" className="btn">Login</button>
-
+            <pre>{JSON.stringify(this.state,null,2)}</pre>
         </form>
       );
     }
