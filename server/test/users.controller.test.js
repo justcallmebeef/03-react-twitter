ccd52068d47eee47971c2856d4b1900d6bc456cb
DylanKojiCheslin <dylan.kojicheslin@gmail.com>
2018-10-31 19:27:37 -0600

    user tests

--- server/test/users.controller.test.js
+++ server/test/users.controller.test.js
@@ -3,12 +3,10 @@ const knex = require('../knex');
 
 
 const { loginUser } = require('../controllers/users');
-// const { createUser } = require('../controllers/users');
+const { createUser } = require('../controllers/users');
 // const { findUserByEmail } = require('../controllers/users');
 // const { findUserByHandle } = require('../controllers/users');
 const { INV_REQ } = require('../controllers/utilities');
-
-let usersToDelete = [];
 
 const runUsersControllerUnitTests = () => {
   describe('loginUser()', () => {
@@ -20,6 +18,51 @@ const runUsersControllerUnitTests = () => {
       await loginUser(req, res, next);
       assert.equal(res.err, INV_REQ)
     });
+
+    it('doe not Log in with incorect handle and password', async() => {
+      const next = () => {};
+      const req = { body: {
+        handle : 'janedoe',
+        password : 'qwertyuiop'
+      } };
+      let res = {};
+      let things = await loginUser(req, res, next);
+      assert.equal(res.err, INV_REQ)
+    });
+
+  });
+
+  describe('createUser()', () => {
+    let usersToDelete = [];
+    it('Inserts user with name, handle, email, password, bio, location, and birthday', async() => {
+      const next = () => {};
+      const req = { body:
+        {
+          name: 'Jay Doe',
+          handle: 'jayedoe',
+          email: 'jaydoe@gmail.com',
+          password: 'drink water',
+          bio: 'I do stuff and things.',
+          location: 'Boulder, CO',
+          birthday: '01/01/1990'
+        }
+      };
+      let res = {};
+      const usersEmail = req.body.email;
+      await createUser(req, res, next);
+      usersToDelete.push(res.data.id);
+      expect(res.data).to.have.all.keys('id', 'name', 'handle', 'email', 'avatar', 'bio', 'location', 'birthday')
+      expect(res.data.email).equal(usersEmail);
+    });
+
+    after(async() => {
+      try {
+        await knex('users').whereIn('id', usersToDelete).del()
+      } catch (err) {
+        console.log('Couldn\'t delete test users: ' + err);
+      }
+    });
+
   });
 };
 
