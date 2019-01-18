1f72962004a84e66e47aa7d1e641e7a6131485fd
Cfunkles <cfunkles@gmail.com>
2019-01-17 19:36:08 -0700

    refactor chucks code to be more reacty, add a sort method and create a usersUtilities.js file

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,35 +1,47 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import ProfileAvatar from './ProfileAvatar';
 import AvatarDialog from './AvatarDialog';
 import { getMessages } from '../../api/messageApi'; 
 
 import './Profile.css';
+import { sortMessagesById } from '../../Utilities/userUtilities';
+import { addUsersStars } from '../../Utilities/userUtilities';
 
 
 class Profile extends Component {
-  state = {
-    avatar: 'https://via.placeholder.com/150x150',
-    name: 'Paul',
-    handle: '@jack',
-    message_count: this.getMessages(),
-    // star_count: '73',
-    bio: 'Front end dev located in Denver',
-    location: 'Denver, CO',
-    link: 'github.com/git',
-    birth_date: '03/02/1999',
-    dialog_open: false
+  constructor()  {
+    super();
+    this.state = {
+      avatar: 'https://via.placeholder.com/150x150',
+      name: 'Paul',
+      handle: '@jack',
+      message_count: 0,
+      star_count: 0,
+      bio: 'Front end dev located in Denver',
+      location: 'Denver, CO',
+      link: 'github.com/git',
+      birth_date: '03/02/1999',
+      dialog_open: false
+    };
   }
 
-  getMessages() {
+  componentDidMount() {
+    this.initializeUserDashboard();
+  }
+
+  initializeUserDashboard = () => {
     getMessages().then(res => {
-      this.setState({star_count: this.addStars(res.data)});
-      this.setState({message_count: res.data.length});
-      this.setState({messages: this.renderMessageItem(res.data)});
+      sortMessagesById(res.data);
+      this.setState({
+        star_count: addUsersStars(res.data),
+        message_count: res.data.length,
+        messages: this.renderMessageItem(res.data)
+      });
     });
   }
 
-  renderMessageItem(messagesList) {
+  renderMessageItem = (messagesList) => {
     let htmlList = [];
     // TODO Sort array by time stamp, once time stamp is getting here from data.
     messagesList.forEach((message, index) => {
@@ -38,15 +50,7 @@ class Profile extends Component {
     return htmlList;
   }
 
-  addStars(messagesList) {
-    let stars = 0;
-    messagesList.forEach(message => {
-      stars += message.stars;
-    });
-    return stars;
-  }
-
-  avatarClicked(e) {
+  avatarClicked = (e) => {
      e.preventDefault();
      console.log('The link was clicked.');
   }
