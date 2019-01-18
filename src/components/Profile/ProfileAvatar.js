3584af54f1a3447e1281bb66190befdad33684bb
Cole Hart <cole@colehart.com>
2019-01-17 19:16:26 -0700

    Add alt prop to img tag

--- src/components/Profile/ProfileAvatar.js
+++ src/components/Profile/ProfileAvatar.js
@@ -2,7 +2,11 @@ import React from 'react'
 
 const ProfileAvatar = props => {
   return (
-    <img onClick={props.onClick} src={props.image} style={{
+    <img
+      onClick={props.onClick}
+      src={props.image}
+      alt='The profile avatar'
+      style={{
       border: '2px solid #333',
       borderRadius: '50px',
       height: '150px',
