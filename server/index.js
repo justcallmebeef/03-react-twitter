ba1787a9de895e39a6ce782c0ccc1a64bf86119c
Cole Hart <cole@colehart.com>
2019-01-19 09:47:52 -0700

    Fixes error thrown in test suite when running test suite simultaneously with server and client

--- server/index.js
+++ server/index.js
@@ -44,6 +44,6 @@ app.use('*', (req, res) => {
   else res.send({ data: res.data });
 });
 
-app.listen(process.env.PORT || 8080);
+if (!module.parent) app.listen(process.env.PORT || 8080);
 
 module.exports = app;
