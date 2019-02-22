fc3e37de9c49d36ad2f4b236f6465ee5e138ac50
Jean Paul Cornejo <jeanpaulcornejo@gmail.com>
2019-02-21 20:34:26 -0700

    Adds change avatar to Account page

--- src/components/Account.js
+++ src/components/Account.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
 import './Account.css'
+import ProfileAvatar from './Profile/ProfileAvatar';
+import AvatarDialog from './Profile/AvatarDialog';
 
 class Account extends Component {
 
+  constructor() {
+    super();
+    this.state = {
+      avatar: 'https://via.placeholder.com/150x150',
+    };
+  }
+
   submitButton = () => {
     alert('Updates Submitted!')
+  }
+
+  changeAvatar = (url) => {
+    console.log('Changing avatar');
+    this.setState({ avatar: url });
   }
 
   render() {
@@ -17,10 +31,12 @@ class Account extends Component {
       <input className="input" placeholder="bruceWayne"></input> <br/>
       <label className="title">Edit Email</label>
       <input className="input" placeholder="bruceWayne@batcave.com"></input> <br/>
+      <ProfileAvatar onClick={this.avatarClicked} image={this.state.avatar} />
+      <AvatarDialog changeAvatar={this.changeAvatar} /> <br />
       <button onClick={this.submitButton} className="btn">Save changes</button>
       </div>
     </div>
     )
-}
+  }
 }
 export default Account;
