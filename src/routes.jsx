5c1da521f73c77bf378499987633d6c193f348fc
Kevin Guebert <kguebert93@gmail.com>
2019-02-21 19:53:48 -0700

    #19 Undoing prettier changes

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,22 +1,22 @@
-import React from "react";
-import { Route, Switch } from "react-router-dom";
-import Account from "./components/Account/Account";
-import Navigation from "./components/Navigation";
-import App from "./components/App";
-import Login from "./components/Login/Login";
-import Register from "./components/Register";
-import SampleReduxContainer from "./components/SampleReduxContainer";
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Account from './components/Account/Account';
+import Navigation from './components/Navigation';
+import App from './components/App';
+import Login from './components/Login/Login';
+import Register from './components/Register';
+import SampleReduxContainer from './components/SampleReduxContainer';
 
 export default (
   <div>
     <Navigation />
     <div>
       <Switch>
-        <Route exact path={"/"} component={App} />
-        <Route path={"/account"} component={Account} />
-        <Route path={"/login"} component={Login} />
-        <Route path={"/register"} component={Register} />
-        <Route path={"/redux"} component={SampleReduxContainer} />
+        <Route exact path={'/'} component={App} />
+        <Route path={'/account'} component={Account} />
+        <Route path={'/login'} component={Login} />
+        <Route path={'/register'} component={Register} />
+        <Route path={'/redux'} component={SampleReduxContainer} />
       </Switch>
     </div>
   </div>
